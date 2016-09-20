a24df126e5763a7029a1e61e40747facf604dfc2
roelcantu <roelhcantu@gmail.com>
2016-09-20 18:56:54 -0500

    Completed Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -29,11 +29,15 @@ let b = 32
 
 
 
-
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
+switch Double(y) / x > 3 {
+case true:
+    print("true")
+case false:
+    print("false")
+}
 
 
 
@@ -43,7 +47,12 @@ let b = 32
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
+switch Double(y) > x && a / b > 9  {
+case true:
+    print("true")
+default:
+    print("false")
+}
 
 
 
@@ -54,8 +63,16 @@ let b = 32
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
+func isGreater(a: Int, b: Int) -> String {
+    switch a > b {
+    case true:
+        return "yep"
+    default:
+        return "nope"
+    }
+}
 
-
+print(isGreater(a: a, b: b))
 
 
 
@@ -70,7 +87,17 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+var Luke = "Strong"
+var Yoda = "Strong"
+func isForce(force: String) -> Bool {
+    if (Luke == force) {
+        return true
+    } else {
+        return false
+    }
+}
 
+print(isForce(force: Yoda))
 
 
 
@@ -86,7 +113,20 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
+func isInRange(num: Int) {
+    switch num {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
 
+isInRange(num: 5)
 
 
 
