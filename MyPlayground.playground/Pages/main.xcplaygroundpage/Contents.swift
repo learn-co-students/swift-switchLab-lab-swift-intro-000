6c809213fae0529b7f5a9cd8888b1fea7a771bcd
jajuki13 <khkim1776@gmail.com>
2016-09-23 20:36:42 -0400

    functions with switch inside

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,54 +15,45 @@ let x = 5.0
 let y = 12
 let a = 321
 let b = 32
-
-
-
-
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
-
-
-
-
-
-
-
-
+switch a % b == 0 {
+case true:
+    print("true")
+case false:
+    print("false")
+}
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
-
-
-
-
-
-
+switch (y / Int(x)) > 3 {
+case true:
+    print("true")
+case false:
+    print("false")
+}
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
-
-
-
-
-
-
-
+switch y > Int(x) && (a / b) > 9 {
+case true:
+    print("true")
+case false:
+    print("false")
+}
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
-
-
-
-
-
-
-
-
-
+func isGreater(arg1: Int, arg2: Int) -> String {
+    switch arg1 > arg2{
+    case true:
+        return "yep"
+    case false:
+        return "nope"
+    }
+}
+//isGreater(arg1: 6, arg2: 4)
 /*: Question 5
 ### **(5)** Write a function named `isForce` that takes a `String` argument which represents a persons name and returns a `Bool`. If the persons name is equal to the name of someone with the force, return `true`, else return `false`. People with the force are Luke, Leia, Anakin, Obi Wan, Yoda & Vader.
  ```swift
@@ -70,14 +61,25 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
-
-
-
-
-
-
-
-
+func isForce(arg1: String) -> Bool {
+    switch arg1 {
+    case "Luke":
+        return true
+    case "Leia":
+        return true
+    case "Anakin":
+        return true
+    case "Obi-Wan":
+        return true
+    case "Yoda":
+        return true
+    case "Darth Vader":
+        return true
+    default:
+        return false
+    }
+}
+//isForce(arg1: "Luke")
 /*: Question 6
 ### **(6)** Write a function "isInRange" that takes an Int  argument and prints "small" if it's in the range of 0 and 3, "medium" if it's in the range of 4 and 6, "big" if it's in the range of 7 and 10, and otherwise prints "not sure".
  ```swift
@@ -87,11 +89,17 @@ let b = 32
  ```
 */
 
-
-
-
-
-
-
-
+func isInRange(arg1: Int) {
+    switch arg1 {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
+//isInRange(arg1: 3)
 //:
