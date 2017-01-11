811fdcb5e1bdafb29622686820b4fe27c3bc554d
Dzung Pham <dzungpham@Dzungs-MacBook-Air.local>
2017-01-11 00:03:48 -0500

    solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -23,45 +23,45 @@ let b = 32
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
 
-
-
-
-
-
-
-
+switch a % b {
+    case 0:
+        print("true")
+    default:
+        print ("false")
+}
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
-
-
-
-
-
-
+switch Double(y)/x > 3 {
+case true:
+    print("true")
+default:
+    print("false")
+}
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
-
-
-
-
-
-
+switch y > Int(x) && b > 9 {
+case true:
+    print("true")
+default:
+    print("false")
+}
 
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
+func isGreater(a: Int, b: Int) -> String {
+    switch a > b{
+    case true:
+        return ("yep")
+    default:
+        return ("nope")
+    }
+}
 
-
-
-
-
-
-
-
+isGreater(a: 3, b: 2)
+isGreater(a: 4, b: 8)
 
 /*: Question 5
 ### **(5)** Write a function named `isForce` that takes a `String` argument which represents a persons name and returns a `Bool`. If the persons name is equal to the name of someone with the force, return `true`, else return `false`. People with the force are Luke, Leia, Anakin, Obi Wan, Yoda & Vader.
@@ -70,14 +70,17 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+func isForce(name: String) -> Bool {
+    switch ["Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader"].contains(name){
+    case true:
+        return true
+    default:
+        return false
+    }
+}
 
-
-
-
-
-
-
-
+isForce(name: "Anakin")
+isForce(name: "Rachel")
 /*: Question 6
 ### **(6)** Write a function "isInRange" that takes an Int  argument and prints "small" if it's in the range of 0 and 3, "medium" if it's in the range of 4 and 6, "big" if it's in the range of 7 and 10, and otherwise prints "not sure".
  ```swift
@@ -86,9 +89,21 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
+func isInRange(number: Int) {
+    switch number {
+    case 0...3:
+        print ("small")
+    case 4...6:
+        print ("medium")
+    case 7...10:
+        print ("big")
+    default:
+        print ("not sure")
+    }
+}
 
-
-
+isInRange(number: 4)
+isInRange(number: 19)
 
 
 
