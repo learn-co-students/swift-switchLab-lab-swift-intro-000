12a5456a1ea91591f0f3af820d5caa27cc9ea263
Brian Harrington <brianharrington715@gmail.com>
2017-04-10 23:29:13 -0700

    Completed lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -22,28 +22,34 @@ let b = 32
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
-
-
-
-
-
-
+switch a % b == 0 {
+case true:
+    print("true")
+default:
+    print("false")
+}
 
 
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
-
-
-
-
+switch y / Int(x) {
+case 3..<Int.max:
+    print("true")
+default:
+    print("false")
+}
 
 
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
+switch y > Int(x) && a / b > 9 {
+case true:
+    print("true")
+default:
+    print("false")
+}
 
 
 
@@ -54,14 +60,14 @@ let b = 32
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
-
-
-
-
-
-
-
-
+func isGreater(a:Int, b:Int) -> String {
+    switch a > b {
+    case true:
+        return "yep"
+    default:
+        return "nope"
+    }
+}
 
 /*: Question 5
 ### **(5)** Write a function named `isForce` that takes a `String` argument which represents a persons name and returns a `Bool`. If the persons name is equal to the name of someone with the force, return `true`, else return `false`. People with the force are Luke, Leia, Anakin, Obi Wan, Yoda & Vader.
@@ -70,13 +76,18 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+func isForce(with: String) -> Bool {
+    switch with {
+    case "Luke","Leia","Anakin", "Obi Wan","Yoda","Vader":
+        return true
+    default:
+        return false
+    }
+}
 
-
-
-
-
-
-
+isForce(with: "Luke")
+isForce(with: "Sam")
+isForce(with: "Goku")
 
 /*: Question 6
 ### **(6)** Write a function "isInRange" that takes an Int  argument and prints "small" if it's in the range of 0 and 3, "medium" if it's in the range of 4 and 6, "big" if it's in the range of 7 and 10, and otherwise prints "not sure".
@@ -86,12 +97,21 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
+func isInRange(a: Int) {
+    switch a {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
 
-
-
-
-
-
-
+isInRange(a: 0)
+isInRange(a: 99)
+isInRange(a: 8)
 
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-switchLab-lab/blob/solution/MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
