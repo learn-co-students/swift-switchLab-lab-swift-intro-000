fbba119cbb922c65cfa9ae68b50ebf583b2fa1a6
Marc Lacerna <marcyvan.lacenra@gmail.com>
2017-04-15 17:52:36 -0400

    firts commit

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -22,6 +22,12 @@ let b = 32
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
+switch a % b == 0 {
+    case true:
+        print("true")
+    case false:
+        print ("false")
+}
 
 
 
@@ -33,7 +39,12 @@ let b = 32
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
+switch y / Int(x) > 3 {
+case true:
+    print("true")
+case false:
+    print ("false")
+}
 
 
 
@@ -44,7 +55,12 @@ let b = 32
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
 
-
+switch y > Int(x) && (a / b) > 9 {
+case true:
+    print("true")
+case false:
+    print ("false")
+}
 
 
 
@@ -55,9 +71,19 @@ let b = 32
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
 
+func isGreater(a: Int, b: Int) -> String {
+    switch a > b {
+    case true:
+        return "yep"
+    case false:
+        return "nope"
+    }
+}
 
 
 
+
+isGreater(a: 1, b: 10)
 
 
 
@@ -70,13 +96,23 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+func isForce(with: String) -> Bool {
+    switch with {
+    case "Luke", "Leia", "Anakin", "ObiWan", "Yoda", "Vader":
+        return true
+    default:
+        return false
+        
+    }
+}
 
 
+let names = ["Luke", "Mary", "Leia", "Marc", "Anakin", "ObiWan","Yoda","Vader", "marky-mark"]
 
-
-
-
-
+for  i in names {
+    print ("\(i) : \(isForce(with: i))")
+    
+}
 
 /*: Question 6
 ### **(6)** Write a function "isInRange" that takes an Int  argument and prints "small" if it's in the range of 0 and 3, "medium" if it's in the range of 4 and 6, "big" if it's in the range of 7 and 10, and otherwise prints "not sure".
@@ -86,6 +122,23 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
+func isInRange(arg: Int){
+    switch arg{
+    case 0 ... 3:
+        print("small")
+        
+    case 4 ... 6:
+        print("medium")
+        
+    case 7...10:
+        print("big")
+        
+    default:
+        print("not sure")
+    }
+}
+
+isInRange(arg: 10)
 
 
 
