09fbd047b075a99e15a3c2fd54695315be05af6f
dgmalot <dgmalot@gmail.com>
2017-07-13 17:38:33 -0400

    Completed all questions.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,10 +11,10 @@
 //: First let's try some of last lab's questions but using a 'switch' instead of 'if' or 'if/else'. Can you figure out the types of `x`, `y`, `a`, and `b` without option clicking the constants?
 
 // Given....
-let x = 5.0
-let y = 12
-let a = 321
-let b = 32
+let x = 5.0     // Double
+let y = 12      // Int
+let a = 321     // Int
+let b = 32      // Int
 
 
 
@@ -22,10 +22,10 @@ let b = 32
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
-
-
-
-
+switch a % b {
+    case 0: print("true")
+    default: break
+}
 
 
 
@@ -33,33 +33,34 @@ let b = 32
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
-
-
-
-
+switch Double(y) / x > 3.0 {
+    case true: print("true")
+    default: print("false")
+}
 
 
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
-
-
-
-
+switch (Double(y) > x && a / b > 9) {
+case true: print("true")
+default: break
+}
 
 
 
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
+func isGreater(a: Int, b: Int) -> String {
+    switch a > b {
+    case true: return "Yep"
+    default: return "Nope"
+    }
+}
 
-
-
-
-
-
+print(isGreater(a: a, b: b))
+print(isGreater(a: b, b: a))
 
 
 
@@ -70,9 +71,17 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+func isForce(with name: String) -> Bool {
+    switch name {
+        case "Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader":
+            return true
+        default:
+            return false
+    }
+}
 
-
-
+print(isForce(with: "Luke"))
+print(isForce(with: "Dave"))
 
 
 
@@ -86,6 +95,24 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
+func isInRange(_ testNumber: Int) {
+    switch testNumber {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
+
+isInRange(0)
+isInRange(99)
+isInRange(5)
+isInRange(8)
+
 
 
 
