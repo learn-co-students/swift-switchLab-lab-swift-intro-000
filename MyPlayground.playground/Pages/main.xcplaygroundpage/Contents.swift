96fac9677ab687ccb1cdbb1c45eb8d809c19c711
tormic <tormic1992@gmail.com>
2017-07-26 20:32:05 +0200

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -22,8 +22,14 @@ let b = 32
 /*: Question 1
 ### **(1)** Print "true" if a modulo b is equal to zero.
 */
+var c = a % b
 
-
+switch c  {
+case 0:
+    print("true")
+default:
+    print("false")
+}
 
 
 
@@ -33,7 +39,14 @@ let b = 32
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
+var d = Double(y)/x
+switch d>3 {
+case true:
+    print("true")
+case false:
+    print("false")
+    
+}
 
 
 
@@ -43,7 +56,12 @@ let b = 32
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
+switch y > Int(x) && a / b > 9 {
+case true:
+    print("true")
+default:
+    print("Ain't right")
+}
 
 
 
@@ -54,10 +72,14 @@ let b = 32
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
-
-
-
-
+func isGreater(a: Int, b: Int) -> String {
+    switch a > b {
+    case true:
+        return "yep"
+    default:
+    return "nope"
+    }
+}
 
 
 
@@ -71,7 +93,14 @@ let b = 32
  ```
 */
 
-
+func isForce(name: String) ->Bool {
+    switch name {
+        case "Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader":
+        return true
+    default:
+    return false
+    }
+}
 
 
 
@@ -86,7 +115,18 @@ let b = 32
  isInRange(5) // prints "big"
  ```
 */
-
+func isInRange(arg: Int) {
+    switch arg {
+    case 0...3:
+        print("small")
+    case 4...6:
+        print("medium")
+    case 7...10:
+        print("big")
+    default:
+        print("not sure")
+    }
+}
 
 
 
